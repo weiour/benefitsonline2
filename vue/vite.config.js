ca7e2a3af5517152e93814413b591b42dbf00ac8
weiour <makcum228811@gmail.com>
2025-11-29 06:02:26 +0900

    fin

--- vue/vite.config.js
+++ vue/vite.config.js
@@ -3,8 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/',
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 })
