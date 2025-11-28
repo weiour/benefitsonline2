4761070dc1706a05b593ce77873bd82f0378c66b
weiour <makcum228811@gmail.com>
2025-11-29 05:11:45 +0900

    fin

---
+++ vue/dist/sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'benefits-cache-v2';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  console.log('Service Worker installed');
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      console.log('Service Worker activated');
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  return;
+});
